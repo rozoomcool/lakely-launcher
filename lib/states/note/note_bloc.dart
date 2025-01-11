a0398e43a586817044e8bfc361c2a06527ae1b5b
rozoomcool <rozoom.um@gmail.com>
2025-01-11 23:11:56 +0300

    change state type

--- lib/states/note/note_bloc.dart
+++ lib/states/note/note_bloc.dart
@@ -11,110 +11,66 @@ part 'note_event.dart';
 class NoteBloc extends Bloc<NoteEvent, NoteState> {
   final NotesService _notesService;
 
-  NoteBloc(this._notesService) : super(NoteInitial()) {
+  NoteBloc(this._notesService) : super(const NoteState.empty()) {
     on<LoadNote>(_onLoadNote);
-    // on<InitializeNewNote>(_onInitializeNewNote);
     on<UpdateNote>(_onUpdateNote);
-    // on<UpdateNoteContent>(_onUpdateNoteContent);
-    // on<AutoSaveNote>(_onAutoSaveNote);
     on<SaveNote>(_onSaveNote);
   }
 
   Future<void> _onLoadNote(LoadNote event, Emitter<NoteState> emit) async {
-    emit(NoteLoading());
+    emit(state.copyToLoading());
     try {
       if (event.noteId == null) {
-        emit(NoteEmpty());
+        emit(const NoteState.empty());
         return;
       }
+
       final note = _notesService.getNoteById(event.noteId!);
       if (note == null) {
-        emit(NoteError("Заметка не найдена"));
+        emit(state.copyToError("Заметка не найдена"));
         return;
       }
-      emit(NoteLoaded(note));
+
+      emit(state.copyToLoaded(note));
     } catch (e) {
-      emit(NoteError('Не удалось загрузить заметку.'));
+      emit(state.copyToError('Не удалось загрузить заметку.'));
     }
   }
 
   void _onUpdateNote(UpdateNote event, Emitter<NoteState> emit) {
-    if (state is NoteEmpty) {
+    if (state.note == null) {
+      // Создаем новую заметку, если она еще не существует
       final noteId =
           _notesService.addNote(event.title ?? "", event.content ?? "");
       final note = _notesService.getNoteById(noteId)!;
-      emit(NoteLoaded(note));
-    }
-    if (state is NoteModified) {
-      final currentState = state as NoteModified;
-      final updatedNote = currentState.note
-        ..title = event.title ?? currentState.note.title
-        ..content = event.content ?? currentState.note.content;
-      emit(NoteModified(updatedNote));
-    }
-    if (state is NoteLoaded) {
-      final currentState = state as NoteLoaded;
-      final updatedNote = currentState.note
-        ..title = event.title ?? currentState.note.title
-        ..content = event.content ?? currentState.note.content;
-      emit(NoteModified(updatedNote));
+      emit(state.copyToLoaded(note));
+    } else {
+      // Обновляем текущую заметку
+      final updatedNote = state.note!
+        ..title = event.title ?? state.note!.title
+        ..content = event.content ?? state.note!.content;
+      emit(state.copyWith(note: updatedNote, isModified: true));
     }
   }
 
   Future<void> _onSaveNote(SaveNote event, Emitter<NoteState> emit) async {
-    if (state is NoteModified) {
-      final currentState = state as NoteModified;
-      final isUpdated = _notesService.updateNote(currentState.note.id,
-          title: currentState.note.title, content: currentState.note.content);
-      if (isUpdated){
-        final note = _notesService.getNoteById(currentState.note.id)!;
-        emit(NoteLoaded(note));
+    if (state.note != null && state.isModified) {
+      try {
+        final updated = _notesService.updateNote(
+          state.note!.id,
+          title: state.note!.title,
+          content: state.note!.content,
+        );
+
+        if (updated) {
+          final note = _notesService.getNoteById(state.note!.id)!;
+          emit(state.copyToLoaded(note));
+        } else {
+          emit(state.copyToError("Не удалось сохранить заметку."));
+        }
+      } catch (e) {
+        emit(state.copyToError("Ошибка при сохранении заметки."));
       }
     }
   }
-
-// void _onUpdateNoteTitle(UpdateNoteTitle event, Emitter<NoteState> emit) {
-//   if (state is NoteEmpty) {
-//     final noteId = _notesService.addNote(event.title, "");
-//     final note = _notesService.getNoteById(noteId)!;
-//     emit(NoteLoaded(note));
-//   }
-//   if (state is NoteModified) {
-//     final currentState = state as NoteLoaded;
-//     final updatedNote = currentState.note..title = event.title;
-//     emit(NoteLoaded(updatedNote));
-//   }
-// }
-//
-// void _onUpdateNoteContent(UpdateNoteContent event, Emitter<NoteState> emit) {
-//   if (state is NoteEmpty) {
-//     final noteId = _notesService.addNote("", event.content);
-//     final note = _notesService.getNoteById(noteId)!;
-//     emit(NoteLoaded(note));
-//   }
-//   if (state is NoteModified) {
-//     final currentState = state as NoteLoaded;
-//     final updatedNote = currentState.note..content = event.content;
-//     emit(NoteLoaded(updatedNote));
-//   }
-// }
-
-// void _onInitializeNewNote(
-//     InitializeNewNote event, Emitter<NoteState> emit) {
-//   if (state is NoteInitial) {
-//     final newNote = Note(
-//       id: DateTime.now().toIso8601String(),
-//       title: '',
-//       content: '',
-//     );
-//     emit(NoteLoaded(newNote));
-//   }
-// }
-//
-//
-// Future<void> _onAutoSaveNote(AutoSaveNote event, Emitter<NoteState> emit) async {
-//   if (state is NoteLoaded) {
-//     print('Note auto-saved!');
-//   }
-// }
 }
--- lib/states/note/note_state.dart
+++ lib/states/note/note_state.dart
@@ -1,41 +1,51 @@
 part of "note_bloc.dart";
 
-abstract class NoteState extends Equatable {
-  const NoteState();
+class NoteError extends Equatable {
+  final String? message;
+
+  const NoteError({this.message});
 
   @override
-  List<Object?> get props => [];
-}
-
-class NoteInitial extends NoteState {}
-
-class NoteLoading extends NoteState {}
-
-class NoteEmpty extends NoteState {}
-
-class NoteLoaded extends NoteState {
-  final Note note;
-
-  const NoteLoaded(this.note);
-
-  @override
-  List<Object?> get props => [note];
-}
-
-class NoteModified extends NoteState {
-  final Note note;
-
-  const NoteModified(this.note);
-
-  @override
-  List<Object?> get props => [note];
-}
-
-class NoteError extends NoteState {
-  final String message;
-
-  const NoteError(this.message);
-
-  @override
+  // TODO: implement props
   List<Object?> get props => [message];
+}
+
+class NoteState extends Equatable {
+  const NoteState(this.note, this.isLoading, this.error, this.isModified);
+
+  const NoteState.empty(
+      {this.note, this.isLoading = false, this.error, this.isModified = false});
+
+  final Note? note;
+  final bool isLoading;
+  final NoteError? error;
+  final bool isModified;
+
+  NoteState copyWith(
+      {Note? note, bool? isLoading, NoteError? error, bool? isModified}) {
+    return NoteState(note ?? this.note, isLoading ?? this.isLoading,
+        error ?? this.error, isModified ?? this.isModified);
+  }
+
+  NoteState copyToError(String message) {
+    return copyWith(
+        error: NoteError(message: message),
+        isLoading: false,
+        isModified: false);
+  }
+
+  NoteState copyToLoading() {
+    return copyWith(error: null, isLoading: true, isModified: false);
+  }
+
+  NoteState copyToModified() {
+    return copyWith(error: null, isLoading: false, isModified: true);
+  }
+
+  NoteState copyToLoaded(Note note) {
+    return copyWith(note: note, error: null, isLoading: false, isModified: true);
+  }
+
+  @override
+  List<Object?> get props => [note, isLoading, error, isModified];
 }
